63577f1ecc0cd22abd0f7f245b6e4387a1d2eba7
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-08-01 14:37:55 +0100

    Adding quotes array

--- project-1.js
+++ project-1.js
@@ -5,28 +5,34 @@ let quote = document.querySelector('.quote');
 let person = document.querySelector('.person');
 
 const quotes =  [{
-    quote: `"The best way to find yourself is to lose yourself in the service of others"`,
+    quote: `"The best way to find yourself is to lose yourself in the service of others."`,
     person: `Mahatma Gandhi`
 }, {
-    quote: `"If you want to live a happy life, tie it to a goal, not to people or things"`,
+    quote: `"If you want to live a happy life, tie it to a goal, not to people or things."`,
     person: `Albert Einstein`
 }, {
-    quote: `"at his best, man is the noblest of all animals; seperated from law and justice he is th worst"`,
+    quote: `"at his best, man is the noblest of all animals; seperated from law and justice he is th worst."`,
     person: `Aristotle`
 }, {
-    quote: `"Your time is limited, so don't waste it living someone else's life"`,
+    quote: `"Your time is limited, so don't waste it living someone else's life."`,
     person: `Steve Jobs`
 }, {
-    quote: `"Tell me and I forget. Teach me and I remember. Involve me and I learn"`,
+    quote: `"Tell me and I forget. Teach me and I remember. Involve me and I learn."`,
     person: `Benjamin Franklin`
 }, {
-    quote: `"If you look at waht you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough"`,
+    quote: `"If you look at waht you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."`,
     person: `Oprah Winfrey`
 }, {
-    quote: `"Your time is limited, so don't waste it living someone else's life"`,
-    person: `Steve Jobs`
-}
-
-
-
+    quote: `"It doesn't matter how slowly you go, as long as you do not stop."`,
+    person: `Confucius`
+}, {
+    quote: `"Our lives begin to end the day we become silent about things that matter."`,
+    person: `Martin Luther King Jr.`
+}, {
+    quote: `"Remember that not getting what you want is sometimes a wonderful stroke of luck."`,
+    person: `Dalai Lama`
+}, {
+    quote: `"The journey of a thousand miles begins with one step."`,
+    person: `Lao Tzu`
+}, 
 ]
